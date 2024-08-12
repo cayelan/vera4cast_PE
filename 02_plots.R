47265340bf707765f2e989d31b04c1a1180f8683
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-08-12 18:34:51 -0400

    update plotting

--- 02_plots.R
+++ 02_plots.R
@@ -17,6 +17,16 @@ targets_P1D |>
 
 
 ### Figure 2 - PE distributions ####
+summary_PE <- targets_P1D_interp |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL'))) |> 
+  group_by(site_id, variable, depth_m) |> 
+  summarise(PE = calculate_PE(observation, D = D, tau = tau))
+
 PE_resampled_P1D |> 
   mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
          variable = factor(variable, levels = c('Temp_C',
@@ -24,7 +34,9 @@ PE_resampled_P1D |>
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL'))) |> 
-  ggplot() +
+  ggplot()+
+  geom_text(data = summary_PE, 
+            aes(x = PE, y = 8, label = "*", colour = variable), show.legend = F, size = 7) +
   geom_density(aes(x=PE, 
                    colour = variable, 
                    fill = variable), 
@@ -32,12 +44,12 @@ PE_resampled_P1D |>
   facet_grid(depth_m~site_id) +
   scale_fill_viridis_d(name = 'Variable_unit', option = 'plasma') +
   scale_colour_viridis_d(name = 'Variable_unit', option = 'plasma') +
-  scale_x_continuous(expand = c(0,0))+
-  scale_y_continuous(expand = c(0,0))+
+  scale_x_continuous(expand = c(0.01,0.01))+
+  scale_y_continuous(expand = c(0,0), limits = c(0,10), name = 'density')+
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         legend.position = 'right', 
-        strip.background = element_rect(fill = 'white'))
+        strip.background = element_rect(fill = 'white')) 
 
 
 ### Figure 3 - time series ####
@@ -59,16 +71,25 @@ PE_ts_P1D |>
 
 PE_ts_P1D |> 
   filter(year(date) > 2018,
-         variable != 'SpCond_uScm') |> 
+         variable != 'SpCond_uScm') |>  
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL'))) |> 
   ggplot() +
   geom_line(aes(x=yday(date), y=PE, group = interaction(depth_m, year(date)), colour = depth_m),
             alpha = 0.6) +
   geom_smooth(aes(x=yday(date), y=PE, colour = depth_m), 
               method = 'gam', 
               formula = y ~ s(x, bs = "cs")) + 
-  facet_grid(site_id~variable) +
+  facet_grid(variable~site_id) +
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         legend.position = 'right', 
-        strip.background = element_rect(fill = 'white'))
+        strip.background = element_rect(fill = 'white')) +
+  scale_colour_viridis_d(name = '', begin = 0.8, end = 0.4, option = 'viridis') +
+  scale_x_continuous(name = 'day of year', breaks = seq(0,350,50))
+  
 
