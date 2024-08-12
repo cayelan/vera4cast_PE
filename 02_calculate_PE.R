1556f9647cb6bf8deeae476b7eaa26ece101cf7e
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-08-12 13:42:24 -0400

    calculate PE from targets

---
+++ 02_calculate_PE.R
@@ -0,0 +1,27 @@
+# =====================================================#
+# Set parameters for PE calculations
+D  <- 3 # length of the word, embedding dimension
+tau <- 1 # embedding time delay
+window_length <- 30 # for a rolling PE how long should the time series be
+# resample_n <- 50 # how many samples? for the resampled PE
+
+#======================================================#
+
+
+PE_resampled_P1D <- targets_P1D_resample |> 
+  group_by(n, site_id, variable, depth_m, doy) |> 
+  summarise(PE = calculate_PE(observation, D = D, tau = tau, use_weights = T, ignore_gaps = T),
+            .groups = 'drop')
+
+#================================#
+PE_ts_P1D <- targets_P1D_interp |> 
+  group_by(site_id, variable, depth_m) |> 
+  group_split() |> 
+  map(.f = ~calculate_PE_ts(x = .x,
+                            time_col = 'date', 
+                            window_width = 50,
+                            tie_method = 'first', 
+                            D = 3, tau = 1, 
+                            use_weights = T)) |> 
+  bind_rows()
+
