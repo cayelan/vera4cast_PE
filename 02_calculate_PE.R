43d5d5ae4549f63a16d416b475dbe36636938652
Freya E Olsson <freyao@tc011.cm.cluster>
2024-08-13 11:54:38 -0400

    calculate PE on shuffled ts

--- 02_calculate_PE.R
+++ 02_calculate_PE.R
@@ -7,6 +7,18 @@ window_length <- 30 # for a rolling PE how long should the time series be
 
 #======================================================#
 
+summary_PE <- targets_P1D_interp |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL'))) |> 
+  group_by(site_id, variable, depth_m) |> 
+  summarise(PE = calculate_PE(observation, D = D, tau = tau))
+
+#======================================================#
+
 PE_resampled_P1D <- targets_P1D_resample |> 
   group_by(n, site_id, variable, depth_m, doy) |> 
   summarise(PE = calculate_PE(observation, D = D, tau = tau, use_weights = T, ignore_gaps = T),
