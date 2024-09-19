5c47127d4a1e627625a189109e23b9721c824dae
Freya E Olsson <freyao@tc009.cm.cluster>
2024-09-19 14:28:56 -0400

    combined plot of PE across lakes with medians

--- 02_plots.R
+++ 02_plots.R
@@ -20,8 +20,9 @@ targets_P1D |>
 
 ### Figure 2 - PE distributions ####
 summary_PE <- targets_P1D_interp |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Temp_C',
+  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
+         variable = factor(variable, levels = c('AirTemp_C',
+                                                'Temp_C',
                                                 'SpCond_uScm',
                                                 'fDOM_QSU',
                                                 'DO_mgL',
@@ -29,22 +30,42 @@ summary_PE <- targets_P1D_interp |>
   group_by(site_id, variable, depth_m) |> 
   summarise(PE = calculate_PE(observation, D = D, tau = tau))
 
-PE_resampled_P1D |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Temp_C',
+central_tendancy <- PE_resampled_P1D |> 
+  filter(depth_m != 'met') |> 
+  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
+         variable = factor(variable, levels = c('AirTemp_C',
+                                                'Temp_C',
                                                 'SpCond_uScm',
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL')),
          predictability = 1-PE) |> 
+  reframe(.by = c(variable, depth_m, site_id),
+          quantile_80 = quantile(PE, 0.8),
+          CDF = mean(PE <= 0.5),
+          median = median(PE)) 
+
+PE_combined <- PE_resampled_P1D |> 
+  filter(depth_m != 'met') |> 
+  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
+         variable = factor(variable, levels = c('AirTemp_C',
+                                                'Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL')),
+         predictability = 1-PE) |> 
+  na.omit() |> 
   ggplot()+
-  geom_density_ridges(aes(x=predictability, y= variable,
+  labs(title = 'sample') +
+  geom_density_ridges(aes(x=PE, y= variable,
                    colour = variable, 
                    fill = variable), 
                alpha = 0.5, rel_min_height = 0.005) +
   # geom_text(data = summary_PE, 
   #           aes(x = PE, y = variable, label = "*", colour = variable), show.legend = F, size = 7) +
-  geom_vline(data = summary_PE, aes(xintercept = 1-PE, colour = variable), 
+  geom_vline(data = filter(summary_PE, depth_m != 'met'),
+             aes(xintercept = PE, colour = variable), 
              show.legend = F, linewidth = 0.8, alpha = 0.7, linetype = 'longdash') +
   facet_grid(depth_m~site_id, scales = 'free') +
   scale_fill_viridis_d(name = 'Variable_unit', option = 'plasma', begin = 0, end = 0.8) +
@@ -56,6 +77,42 @@ PE_resampled_P1D |>
         axis.text.y = element_text(hjust = 1, vjust = -1),
         axis.title.y = element_blank(),
         strip.background = element_rect(fill = 'white', colour = NA)) 
+
+
+PE_sitewise <- PE_resampled_P1D |> 
+  filter(depth_m != 'met') |> 
+  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
+         variable = factor(variable, levels = c('AirTemp_C',
+                                                'Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL')),
+         predictability = 1-PE) |> 
+  na.omit() |> 
+  ggplot()+
+  geom_density_ridges(aes(x=PE, y= variable,
+                          colour = variable, 
+                          fill = variable), 
+                      alpha = 0.5, rel_min_height = 0.005) +
+  geom_vline(data = filter(central_tendancy, depth_m != 'met'),
+             aes(xintercept = median, colour = variable), 
+             show.legend = F, linewidth = 0.8, alpha = 0.7, linetype = 'longdash') +
+  facet_grid(depth_m~site_id, scales = 'free') +
+  scale_fill_viridis_d(name = 'Variable_unit', option = 'plasma', begin = 0, end = 0.8) +
+  scale_colour_viridis_d(name = 'Variable_unit', option = 'plasma', begin = 0, end = 0.8) +
+  scale_x_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1, 0.2))+
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = 'top', 
+        axis.text.y = element_text(hjust = 1, vjust = -1),
+        axis.title.y = element_blank(),
+        strip.background = element_rect(fill = 'white', colour = NA), 
+        panel.spacing.x = unit(1, "lines")) 
+
+ggpubr::ggarrange(combined, sitewise, common.legend = T, 
+                  widths = c(1,1.75), 
+                  labels = c('a)', 'b)'))
 
 
 ### Figure 3 - time series ####
@@ -76,7 +133,8 @@ PE_ts_P1D |>
 
 
 PE_ts_P1D |> 
-  filter(year(date) > 2018) |>  
+  filter(year(date) > 2020 & site_id == 'BVR' | 
+           year(date) > 2018 & site_id == 'FCR') |>  
   mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
          variable = factor(variable, levels = c('Temp_C',
                                                 'SpCond_uScm',
@@ -103,12 +161,14 @@ PE_ts_P1D |>
 # PE of shuffled realisations
 PE_shuffled_P1D |>
   filter(n %in% 900:1000) |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Temp_C',
+  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
+         variable = factor(variable, levels = c('AirTemp_C',
+                                                'Temp_C',
                                                 'SpCond_uScm',
                                                 'fDOM_QSU',
                                                 'DO_mgL',
-                                                'Chla_ugL'))) |> 
+                                                'Chla_ugL')),
+         predictability = 1-PE) |> 
   ggplot() +
   geom_jitter(aes(y= variable, 
                   x = 1-PE, 
