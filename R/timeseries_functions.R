5aee2e59419a358d5ab94ee26349a049b232c526
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-10-17 19:02:51 -0400

    function to get season

--- R/timeseries_functions.R
+++ R/timeseries_functions.R
@@ -175,3 +175,23 @@ shuffle <- function(ts, times = 1) {
   }
   return(resample_list)
 }
+
+
+#' Get season from date
+#' 
+#' @param ts a vector of data 
+#' @param times how many shuffled realisations should be generated
+#' @returns a dataframe
+season <- function(dates) {
+  WS <- as.Date("2012-12-15", format = "%Y-%m-%d") # Winter Solstice
+  SE <- as.Date("2012-3-15",  format = "%Y-%m-%d") # Spring Equinox
+  SS <- as.Date("2012-6-15",  format = "%Y-%m-%d") # Summer Solstice
+  FE <- as.Date("2012-9-15",  format = "%Y-%m-%d") # Fall Equinox
+  
+  # Convert dates from any year to 2012 dates
+  d <- as.Date(strftime(dates, format="2012-%m-%d"))
+  
+  ifelse (d >= WS | d < SE, "Winter",
+          ifelse (d >= SE & d < SS, "Spring",
+                  ifelse (d >= SS & d < FE, "Summer", "Autumn")))
+}
