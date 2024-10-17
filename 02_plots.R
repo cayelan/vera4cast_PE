02d6f334fba0615ee4a665b287c1dac092db42bf
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2024-10-17 19:02:39 -0400

    season plot

--- 02_plots.R
+++ 02_plots.R
@@ -5,10 +5,11 @@ D = 3
 tau = 1
 ### Figure 1 - observations ####
 targets_P1D |>
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom'))) |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation')) |> 
   ggplot(aes(x=date, y=observation, colour = depth_m)) +
   geom_line(linewidth = 0.8) +
-  facet_grid(variable~site_id, scales = 'free') +
+  facet_grid(variable~site_id + description, scales = 'free') +
   scale_x_date(date_labels = "%d %b %y", breaks = '1 year', name = 'Date') +
   scale_colour_viridis_d(name = '', begin = 0.8, end = 0.4, option = 'viridis') +
   theme_bw() +
@@ -27,8 +28,9 @@ summary_PE <- targets_P1D_interp |>
                                                 'SpCond_uScm',
                                                 'fDOM_QSU',
                                                 'DO_mgL',
-                                                'Chla_ugL'))) |> 
-  group_by(site_id, variable, depth_m) |> 
+                                                'Chla_ugL')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation')) |> 
+  group_by(site_id, variable, depth_m, description) |> 
   summarise(PE = calculate_PE(observation, D = D, tau = tau))
 
 central_tendancy_PE_sitewise <- PE_ts_P1D |> 
@@ -42,11 +44,12 @@ central_tendancy_PE_sitewise <- PE_ts_P1D |>
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation'),
          predictability = 1-PE) |> 
-  reframe(.by = c(variable, depth_m, site_id),
-          quantile_80 = quantile(PE, 0.8),
-          CDF = mean(PE <= 0.5),
-          median = median(PE)) 
+  reframe(.by = c(variable, depth_m, site_id, description),
+          quantile_80 = quantile(predictability, 0.2),
+          CDF = mean(predictability >= 0.5),
+          median = median(predictability)) 
 
 PE_sitewise <- PE_ts_P1D |> 
   filter(depth_m != 'met',
@@ -58,10 +61,11 @@ PE_sitewise <- PE_ts_P1D |>
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation'),
          predictability = 1-PE) |> 
   na.omit() |> 
   ggplot()+
-  geom_density_ridges(aes(x=PE, y= variable,
+  geom_density_ridges(aes(x=predictability, y= variable,
                    colour = variable, 
                    fill = variable), 
                alpha = 0.5, rel_min_height = 0.005) +
@@ -70,7 +74,7 @@ PE_sitewise <- PE_ts_P1D |>
   geom_vline(data = filter(central_tendancy_PE_sitewise),
              aes(xintercept = median, colour = variable), 
              show.legend = F, linewidth = 0.8, alpha = 0.7, linetype = 'longdash') +
-  facet_grid(depth_m~site_id, scales = 'free') +
+  facet_grid(depth_m~site_id + description, scales = 'free') +
   scale_fill_viridis_d(name = 'Variable_unit', option = 'plasma', begin = 0, end = 0.8) +
   scale_colour_viridis_d(name = 'Variable_unit', option = 'plasma', begin = 0, end = 0.8) +
   scale_x_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1, 0.2))+
@@ -92,11 +96,12 @@ central_tendancy_PE_combined <-PE_ts_P1D |>
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL')),
+         description = 'both sites',
          predictability = 1-PE) |> 
   reframe(.by = c(variable, depth_m),
-          quantile_80 = quantile(PE, 0.8),
-          CDF = mean(PE <= 0.5),
-          median = median(PE)) 
+          quantile_80 = quantile(predictability, 0.2),
+          CDF = mean(predictability >= 0.5),
+          median = median(predictability)) 
 
 PE_combined <- PE_ts_P1D |> 
   filter(depth_m != 'met',
@@ -110,9 +115,9 @@ PE_combined <- PE_ts_P1D |>
                                                 'Chla_ugL')),
          predictability = 1-PE) |> 
   na.omit() |> 
-  mutate(sites = 'combined') |> 
+  mutate(sites = 'both sites',) |> 
   ggplot()+
-  geom_density_ridges(aes(x=PE, y= variable,
+  geom_density_ridges(aes(x=predictability, y= variable,
                           colour = variable, 
                           fill = variable), 
                       alpha = 0.5, rel_min_height = 0.005) +
@@ -132,9 +137,64 @@ PE_combined <- PE_ts_P1D |>
         panel.spacing.x = unit(1, "lines")) 
 
 ggpubr::ggarrange(PE_combined, PE_sitewise, common.legend = T, 
-                  widths = c(1,1.75), 
+                  widths = c(1,1.75), align = 'h',
                   labels = c('a)', 'b)'))
 
+### Seasons distributions
+central_tendancy_PE_season <-PE_ts_P1D |> 
+  filter(year(date) >= 2021) |> 
+  na.omit() |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL')), 
+         season = factor(season(date), levels = c('Spring',
+                                                  'Summer',
+                                                  'Autumn',
+                                                  'Winter')),
+         description = 'both sites',
+         predictability = 1-PE) |> 
+  reframe(.by = c(variable, depth_m, season),
+          quantile_80 = quantile(predictability, 0.2),
+          CDF = mean(predictability >= 0.5),
+          median = median(predictability)) 
+
+PE_ts_P1D |> 
+  filter(year(date) >= 2021)  |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL')),
+         predictability = 1-PE, 
+         season = factor(season(date), levels = c('Spring',
+                                                  'Summer',
+                                                  'Autumn',
+                                                  'Winter'))) |> 
+  na.omit() |> 
+  mutate(sites = 'both sites',) |> 
+  ggplot()+
+  geom_density_ridges(aes(x=predictability, y= variable,
+                          colour = variable, 
+                          fill = variable), 
+                      alpha = 0.5, rel_min_height = 0.005) +
+  geom_vline(data = filter(central_tendancy_PE_season),
+             aes(xintercept = median, colour = variable), 
+             show.legend = F, linewidth = 0.8, alpha = 0.7, linetype = 'longdash') +
+  facet_grid(depth_m~season, scales = 'free_y') +
+  scale_fill_viridis_d(name = 'Variable_unit', option = 'turbo', begin = 0.2, end = 1) +
+  scale_colour_viridis_d(name = 'Variable_unit', option = 'turbo', begin = 0.2, end = 1) +
+  scale_x_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1, 0.2))+
+  theme_minimal() +
+  theme(panel.grid.minor = element_blank(),
+        legend.position = 'top', 
+        axis.text.y = element_text(hjust = 1, vjust = -1),
+        axis.title.y = element_blank(),
+        strip.background = element_rect(fill = 'white', colour = NA), 
+        panel.spacing.x = unit(1, "lines")) 
 
 ### Figure 3 - time series ####
 
@@ -147,14 +207,15 @@ PE_ts_P1D |>
                                                 'fDOM_QSU',
                                                 'DO_mgL',
                                                 'Chla_ugL')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation'),
          predictability = 1 - PE) |> 
   ggplot() +
   geom_line(aes(x=yday(date), y=predictability, group = interaction(depth_m, year(date)), colour = depth_m),
             alpha = 0.6) +
   geom_smooth(aes(x=yday(date), y=predictability, colour = depth_m), 
               method = 'gam', 
-              formula = y ~ s(x, bs = "cs")) + 
-  facet_grid(variable~site_id) +
+              formula = y ~ s(x, bs = "cc")) + 
+  facet_grid(variable~site_id + description) +
   theme_bw() +
   theme(panel.grid.minor = element_blank(),
         legend.position = 'right', 
@@ -166,15 +227,13 @@ PE_ts_P1D |>
 
 # PE of shuffled realisations
 PE_shuffled_P1D |>
-  filter(n %in% 900:1000) |> 
-  mutate(depth_m = factor(depth_m, levels = c('met', 'surface', 'bottom')),
-         variable = factor(variable, levels = c('AirTemp_C',
-                                                'Temp_C',
+  filter(n %in% 400:500) |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Temp_C',
                                                 'SpCond_uScm',
                                                 'fDOM_QSU',
                                                 'DO_mgL',
-                                                'Chla_ugL')),
-         predictability = 1-PE) |> 
+                                                'Chla_ugL'))) |> 
   ggplot() +
   geom_jitter(aes(y= variable, 
                   x = 1-PE, 
@@ -186,24 +245,22 @@ PE_shuffled_P1D |>
                    colour = site_id), 
                fill = NA,  position=position_dodge(width=0.9), outlier.shape = NA) +
   facet_wrap(variable~depth_m, scales = 'free') +
-  # facet_nested_wrap(vars(variable, depth_m), 
-  #                   dir = "h",
-  #                   # axes = "x",
-  #                   remove_labels = 'all',
-  #                   ncol = 3,
-  #                   scales = 'free') +
   geom_point(data = summary_PE, 
              aes(y= variable, 
                  x = 1-PE, 
                  colour = site_id), 
              show.legend = F, size = 2.5, shape = 8, 
              position=position_dodge(width = 1))+
-  theme_bw()  +
-  scale_colour_viridis_d(name = '', begin = 0.8, end = 0.4, option = 'mako') +
+  theme_bw()  + 
+  labs(x='predictability') +
+  scale_colour_manual(name = '', values = viridis::mako(n=2, begin = 0.8, end = 0.2), 
+                      labels = c('FCR - with hypolimnetic oxygenation', 
+                                 'BVR - without hypolimentic oxygenation')) +
   theme(panel.grid.minor = element_blank(),
         strip.background = element_rect(fill = 'white'),
         axis.text.y = element_blank(),
         axis.title.y = element_blank(),
         axis.ticks.y = element_blank(),
-        legend.position.inside = c(0.6, 0.15),
+        legend.position.inside = c(0.65, 0.2),
+        legend.position = "inside",
         legend.background = element_rect(fill = "white", colour = NA))
