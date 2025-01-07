95af73eb72e9a4db7f7ab7f5d13c7ea91bc9fae2
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2025-01-07 10:54:21 -0500

    check factor reordering

--- 04_mixed_effects_model.R
+++ 04_mixed_effects_model.R
@@ -23,6 +23,21 @@ model_surface <- lmer(PE ~ site_id + variable + site_id*variable + (site_id*vari
 summary(model_surface)
 car::Anova(model_surface, type = 3, test.statistic = 'F')
 
+
+## ----- Reorder the factors and check output
+# Filter to only surface observations
+PE_ts_P1D_surface_reorder <-  PE_ts_P1D |> 
+  filter(depth_m == 'surface') |> 
+  mutate(variable = factor(variable, ordered = T, levels = c("Tw_C","Chla_ugL", "DO_mgL", "SpCond_uScm",  "fDOM_QSU")))
+# Site_id, variable tests
+# Fit the model for surface variables
+model_surface_reorder <- lmer(PE ~ site_id + variable + site_id*variable + (site_id*variable | day), 
+                      data = PE_ts_P1D_surface_reorder)
+
+# Evaluate model
+summary(model_surface_reorder)
+car::Anova(model_surface_reorder, type = 3, test.statistic = 'F')
+
 # ---- Testing for depth/reservoir/variable differences ----
 # Filter to only DO and Tw (that have both depths and sites)
 PE_ts_P1D_Tw_DO <-  PE_ts_P1D |> 
