6ee09b293142713bf05546bd7d10073c5f6369eb
Freya E Olsson <freyao@tc104.cm.cluster>
2025-01-21 11:39:28 -0500

    remove SI analysis

--- 02_calculate_PE.R
+++ 02_calculate_PE.R
@@ -17,16 +17,6 @@ summary_PE <- targets_P1D_interp |>
   group_by(site_id, variable, depth_m) |> 
   summarise(PE = calculate_PE(observation, D = D, tau = tau))
 
-summary_av_PE <- targets_P1D_av_interp |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Tw_C',
-                                                'SpCond_uScm',
-                                                'fDOM_QSU',
-                                                'DO_mgL',
-                                                'Chla_ugL'))) |> 
-  group_by(site_id, variable, depth_m) |> 
-  summarise(PE = calculate_PE(observation, D = D, tau = tau))
-
 #======================================================#
 
 # PE_resampled_P1D <- targets_P1D_resample |> 
@@ -37,17 +27,6 @@ summary_av_PE <- targets_P1D_av_interp |>
 #================================#
 # Rolling window of PE ---------
 PE_ts_P1D <- targets_P1D_interp |> 
-  group_by(site_id, variable, depth_m) |> 
-  group_split() |> 
-  map(.f = ~calculate_PE_ts(x = .x,
-                            time_col = 'date', 
-                            window_width = window_length,
-                            tie_method = 'first', 
-                            D = 3, tau = 1, 
-                            use_weights = T)) |> 
-  bind_rows()
-
-PE_ts_P1D_av <- targets_P1D_av_interp |> 
   group_by(site_id, variable, depth_m) |> 
   group_split() |> 
   map(.f = ~calculate_PE_ts(x = .x,
