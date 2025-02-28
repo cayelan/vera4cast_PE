f1484e68e7e712cb7d5b7ecf55ca6c16b4b06e8b
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2025-02-28 15:50:15 -0500

    add full PE ts

--- 03_plots.R
+++ 03_plots.R
@@ -175,140 +175,10 @@ ggpubr::ggarrange(PE_combined, PE_sitewise, common.legend = T,
                   widths = c(1,1.75), align = 'h',
                   labels = c('a)', 'b)'))
 
-
-# Figure 5 - PE time series w/ smooth -------------------
-anoxic_length <- function(x, index, threshold = 0) {
-  rle_obj <- rle(na.omit(x) <= threshold)
-    
-  data.frame(lengths = rle_obj$lengths,
-             values = rle_obj$values) |> 
-    mutate(last = cumsum(lengths),
-           first = 1 + last - lengths) |> 
-    filter(values == TRUE) |> 
-    slice_max(lengths) |> 
-    select(first, last) |> 
-    mutate(last = yday(index[last]),
-           first = yday(index[first]))
-  
-}
-
-anoxia_doy <- targets_P1D_interp |>
-  filter(depth_m == 'bottom', variable == 'DO_mgL', site_id == 'BVR') |> 
-  mutate(year = year(date)) |> 
-  reframe(anoxic_length(x = observation, index = date), .by = c(year, depth_m, variable, site_id)) |> 
-  summarise(mean_anoxic_start = median(first),
-            mean_anoxic_end = median(last), .by = c(depth_m, variable, site_id)) |> 
-  mutate(date = as_date('2023-01-01')) # assign a random date so it can be joined and plotted below
-  
-PE_ts_P1D |> 
-  full_join(anoxia_doy, join_by(variable, site_id, depth_m, date))  |>  
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Tw_C',
-                                                'SpCond_uScm',
-                                                'fDOM_QSU',
-                                                'DO_mgL',
-                                                'Chla_ugL')),
-         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation', 'without hypolimnetic oxygenation'),
-         predictability = 1 - PE) |> 
-  ggplot() +
-  geom_rect(aes(xmax = mean_anoxic_end, xmin = mean_anoxic_start, ymax= Inf, ymin = -Inf),
-            alpha = 0.5, fill = 'grey') +
-  geom_line(aes(x=yday(date), y=predictability, group = interaction(depth_m, year(date)), 
-                colour = depth_m),
-            alpha = 0.6) +
-  geom_smooth(aes(x=yday(date), y=predictability, colour = depth_m), 
-              method = 'gam', se = F,
-              formula = y ~ s(x, bs = "cc")) + 
-  facet_grid(variable~site_id + description) +
-  theme_bw() +
-  theme(panel.grid.minor = element_blank(),
-        legend.position = 'right', 
-        strip.background = element_rect(fill = 'white')) +
-  scale_colour_viridis_d(name = '', begin = 0.8, end = 0.4, option = 'viridis') +
-  scale_x_continuous(name = 'day of year', breaks = seq(0,350,50))+
-  scale_y_continuous(name = 'predictability', breaks = seq(0,1,0.2))
-  
-
-
-# Figure 6 - seasonal distributions -------------
-central_tendancy_PE_season <-PE_ts_P1D |> 
-  na.omit() |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Tw_C',
-                                                'SpCond_uScm',
-                                                'fDOM_QSU',
-                                                'DO_mgL',
-                                                'Chla_ugL'), 
-                           labels = c('Tw_C',
-                                      'SpCond_uScm',
-                                      'fDOM_QSU',
-                                      'DO_mgL',
-                                      'Chla_ugL')), 
-         season = factor(season(date), levels = c('Spring',
-                                                  'Summer',
-                                                  'Autumn',
-                                                  'Winter')),
-         description = 'both sites',
-         predictability = 1-PE) |> 
-  reframe(.by = c(variable, depth_m, season),
-          quantile_80 = quantile(predictability, 0.2),
-          CDF = mean(predictability >= 0.5),
-          median = median(predictability)) 
-
-
-# faceted by variable instead of season - allows comparison among seasons within variable
-PE_ts_P1D |> 
-  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
-         variable = factor(variable, levels = c('Tw_C',
-                                                'SpCond_uScm',
-                                                'fDOM_QSU',
-                                                'DO_mgL',
-                                                'Chla_ugL'), 
-                           labels = c('Tw_C',
-                                      'SpCond_uScm',
-                                      'fDOM_QSU',
-                                      'DO_mgL',
-                                      'Chla_ugL')),
-         predictability = 1-PE, 
-         season = factor(season(date), levels = c('Spring',
-                                                  'Summer',
-                                                  'Autumn',
-                                                  'Winter'))) |> 
-  na.omit() |> 
-  mutate(sites = 'both sites') |> 
-  ggplot()+
-  geom_density_ridges(aes(x=predictability, y= fct_rev(season),
-                          colour = season, 
-                          fill = season), 
-                      alpha = 0.5, rel_min_height = 0.005) +
-  geom_vline(data = filter(central_tendancy_PE_season),
-             aes(xintercept = median, colour = season), 
-             show.legend = F, linewidth = 0.8, alpha = 0.9, linetype = 'longdash') +
-  # ggh4x::facet_manual(vars(depth_m,variable), design = design) +
-  ggh4x::facet_grid2(depth_m~variable, drop = T, render_empty = F) +
-  scale_fill_viridis_d(name = 'Season (astronomical)', option = 'turbo', begin = 0, end = 0.8) +
-  scale_colour_viridis_d(name = 'Season (astronomical)', option = 'turbo', begin = 0, end = 0.8) +
-  scale_x_continuous(expand = c(0.01,0.01), limits = c(0,1), breaks = seq(0,1, 0.2))+
-  theme_minimal() +
-  theme(panel.grid.minor = element_blank(),
-        axis.text.y = element_text(hjust = 1, vjust = -1),
-        axis.title.y = element_blank(),
-        strip.background = element_rect(fill = 'white', colour = NA), 
-        panel.spacing.x = unit(1, "lines"),
-        legend.position.inside = c(0.4, 0.2), legend.direction = 'horizontal',
-        legend.position = "inside", legend.title.position = 'top') 
-
+# Figure 5 in 05_gams.R
 
 # Supplementary Info plots -----------------
-# Figure S1 - PE of shuffled realisations -----
-# calculate the p value (how many are more or less than the random)
-full_join(PE_shuffled_P1D, summary_PE,
-          by = join_by(variable, site_id, depth_m),
-          suffix = c('_random', '')) |> 
-  mutate(diff = PE - PE_random) |> # random >= PE, PE not noise
-  group_by(variable, site_id, depth_m) |> 
-  summarise(p = (sum(diff > 0)) / n())
-
+# PE of shuffled realisations -----
 design <- "
  AB#
  DEF
@@ -353,3 +223,31 @@ PE_shuffled_P1D |>
         legend.position.inside = c(0.65, 0.25),
         legend.position = "inside",
         legend.background = element_rect(fill = "white", colour = NA))
+
+# Time series of PE
+
+PE_ts_P1D |> 
+  mutate(depth_m = factor(depth_m, levels = c('surface', 'bottom')),
+         variable = factor(variable, levels = c('Tw_C',
+                                                'SpCond_uScm',
+                                                'fDOM_QSU',
+                                                'DO_mgL',
+                                                'Chla_ugL'), 
+                           labels = c('Tw_C',
+                                      'SpCond_uScm',
+                                      'fDOM_QSU',
+                                      'DO_mgL',
+                                      'Chla_ugL')),
+         description = ifelse(site_id == 'FCR', 'with hypolimnetic oxygenation',
+                              'without hypolimnetic oxygenation'),
+         predictability = 1 - PE) |> 
+  ggplot(aes(x=date, y=predictability, colour = depth_m)) +
+  geom_line(linewidth = 0.8) +
+  facet_grid(variable~site_id + description, scales="free_y") +
+  scale_x_date(date_labels = "%d %b %y", breaks = '1 year', name = 'Date') +
+  scale_colour_viridis_d(name = '', begin = 0.8, end = 0.4, option = 'viridis') +
+  theme_bw() +
+  theme(panel.grid.minor = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = 'top', 
+        strip.background = element_rect(fill = 'white'))
