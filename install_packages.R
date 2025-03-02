d97c6be9791b0dde905ff630e2735c944d4537db
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2025-03-01 19:58:08 -0500

    add stats packages

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,10 @@
 install.packages(c('tidyverse', 'zoo', 'RCurl', 'lubridate', 'tsibble', 'remotes'))
 
 # some plotting packages
-install.packages(c('ggpubr', 'ggridges'))
+install.packages(c('ggpubr', 'ggridges', 'gratia'))
 
 # a plotting package installation from Github (the CRAN version doesn't work atm)
 remotes::install_github('teunbrand/ggh4x')
+
+# required statistical packages
+install.packages(c('car', 'broom', 'car', 'mgcv', 'marginaleffects'))
