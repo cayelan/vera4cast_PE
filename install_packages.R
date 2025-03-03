ceca49c81bf08db2171dcec8636bfadb9b60056e
freya olsson <freyao@vt.edu>
2025-03-03 15:13:03 +0000

    add missed packages

--- install_packages.R
+++ install_packages.R
@@ -19,12 +19,14 @@ install.packages('zoo') # version 1.8.12
 install.packages('RCurl') # version 1.98.1.8
 install.packages('lubridate') # version 1.9.3
 install.packages('tsibble') # version 1.1.5
-install.packages('remotes') # 2.4.2.1
+install.packages('remotes') # version 2.4.2.1
+install.packages('rLakeAnalyzer') # version 1.11.4.1
 
 # Plotting packages
 install.packages('ggpubr') # version 0.6.0
 install.packages('ggridges') # version 0.5.6
 install.packages('gratia') # version 0.9.2
+install.packages('viridis') # version 0.6.2
 # a plotting package installation from Github (the CRAN version doesn't work atm)
 remotes::install_github('teunbrand/ggh4x') # version 0.3.0.9000
 
@@ -32,5 +34,6 @@ remotes::install_github('teunbrand/ggh4x') # version 0.3.0.9000
 install.packages('car') # version 3.1.3
 install.packages('broom') # version 1.0.5
 install.packages('lme4') # version 1.1.36
+install.packages('itsadug') # version 2.4.1
 install.packages('mgcv') # version 1.91
 install.packages('marginaleffects') # version 0.24.0
